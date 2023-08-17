368da0f0e259aef8192c85e5f8811802fc682994
xuefeich <106719196+xuefeich@users.noreply.github.com>
2023-08-17 16:04:39 +0800

    Update tatqa_dataset.py

--- tag_op/data/tatqa_dataset.py
+++ tag_op/data/tatqa_dataset.py
@@ -48,7 +48,7 @@ def convert_start_end_tags(split_tags, paragraph_index):
     return split_tags
 
 def is_whitespace(c):
-    if c == " " or c == "\t" or c == "\r" or c == "\n" or ord(c) == 0x202F:
+    if c == " " or c == "\t" or c == "\r" or c == "\n" or (len(c)==1 and ord(c) == 0x202F):
         return True
     return False
 
