27477db652fc3cfae3eaa96fd80868fb2b871f2d
xuefeich <106719196+xuefeich@users.noreply.github.com>
2023-11-08 17:55:44 +0800

    Update mapping_split.py

--- tag_op/data/mapping_split.py
+++ tag_op/data/mapping_split.py
@@ -21,12 +21,17 @@ def find_ampping(number,table,paragraphs):
     if tm:
         return {"table":[tm]}
     else:
-        for p in paragraphs:
-            
-        para_number = to_number(paras[pid]["text"][mp[0]:mp[1]])
-        if abs(abs(number) - abs(para_number)) < 0.0001 or abs(abs(number) - abs(para_number)*0.01) < 0.0001:
-            temp_mapping["paragraph"] = {}
-            temp_mapping["paragraph"][p] = [mp]
+        for pid in paragraphs:
+            lp = len(paras[pid]["text"])
+            for mp1 in range(lp-1):
+                for mp2 in range(mp1,lp):
+                   para_number = to_number(paras[pid]["text"][mp1:mp2])
+                   if abs(abs(number) - abs(para_number)) < 0.0001 or abs(abs(number) - abs(para_number)*0.01) < 0.0001:
+                       para_mapping = {}
+                       para_mapping["paragraph"] = {}
+                       para_mapping["paragraph"][pid+1] = [[mp1,mp2]]
+                       return para_mapping
+    return None
 
 
 def split_mapping(operands,mapping,table,paras):
