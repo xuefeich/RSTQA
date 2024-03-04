08fe3503dc2954bf42c3ace3c9549bfd4eb0fed0
xuefeich <106719196+xuefeich@users.noreply.github.com>
2024-03-04 16:04:37 +0800

    Update custom_trainer.py

--- tag_op/custom_trainer.py
+++ tag_op/custom_trainer.py
@@ -2,34 +2,10 @@ from transformers import trainer
 
 class TATTrainer(Trainer):
    def compute_f1(self, model, inputs):
-        if self.label_smoother is not None and "labels" in inputs:
-            labels = inputs.pop("labels")
-        else:
-            labels = None
-        outputs = model(**inputs)
-        if self.args.past_index >= 0:
-            self._past = outputs[self.args.past_index]
-
-        if labels is not None:
-            unwrapped_model = unwrap_model(model)
-            if _is_peft_model(unwrapped_model):
-                model_name = unwrapped_model.base_model.model._get_name()
-            else:
-                model_name = unwrapped_model._get_name()
-            if model_name in MODEL_FOR_CAUSAL_LM_MAPPING_NAMES.values():
-                loss = self.label_smoother(outputs, labels, shift_labels=True)
-            else:
-                loss = self.label_smoother(outputs, labels)
-        else:
-            if isinstance(outputs, dict) and "loss" not in outputs:
-                raise ValueError(
-                    "The model did not return a loss from the inputs, only the following keys: "
-                    f"{','.join(outputs.keys())}. For reference, the inputs it received are {','.join(inputs.keys())}."
-                )
-            # We don't use .loss here since the model may return tuples instead of ModelOutput.
-            loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
-
-        return (loss, outputs) if return_outputs else loss
+        labels = inputs.pop("labels")
+        outputs = model.predict(**inputs)
+        
+        return loss
 
    def prediction_step(
         self,
