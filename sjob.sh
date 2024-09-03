2ecbfee37c32690ec7de807a1967b1e819417b64
xuefeich <106719196+xuefeich@users.noreply.github.com>
2024-09-04 06:49:43 +0800

    Update sjob.sh

--- sjob.sh
+++ sjob.sh
@@ -8,7 +8,7 @@
 #SBATCH --partition             v100_batch
 source ~/.bashrc
 source activate alan
-#PYTHONPATH=$PYTHONPATH:$(pwd):$(pwd)/tag_op python tag_op/prepare_dataset.py --mode train --num_arithmetic_operators 6
+#PYTHONPATH=$PYTHONPATH:$(pwd):$(pwd)/tag_op python tag_op/prepare_dataset.py --mode train --num_arithmetic_operators 6 --encoder deberta
 #export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:3072'
 #PYTHONPATH=$PYTHONPATH:$(pwd) deepspeed --num_gpus=4 --no_local_rank tag_op/train_dp.py --data_dir tag_op/cache/ --save_dir ./checkpoint --batch_size 48 --eval_batch_size 8 --max_epoch 50 --warmup 0.06 --optimizer adam --learning_rate 5e-4 --weight_decay 5e-5 --seed 123 --gradient_accumulation_steps 12 --bert_learning_rate 1.5e-5 --bert_weight_decay 0.01 --log_per_updates 50 --eps 1e-6 --encoder roberta --num_ops 6 --gpu_num 4 --deepspeed_config dp_stage3.json
 
